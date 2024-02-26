04008d8081ad2738c51ea3175e95dfd1762f543d
RahulGautamSingh <rahultesnik@gmail.com>
2024-02-26 12:02:53 +0545

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import HomePage from "./routes/Homepage.jsx";
+import ErrorPage from "./error-page.jsx";
+import BusList from "./routes/BusList.jsx";
+import TicketDetails from "./routes/ticketDetails.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  // protect this router such that it can only be reached from the homepage
+  {
+    path: "/buslist",
+    element: <BusList />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/ticket-details",
+    element: <TicketDetails />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/Homepage.jsx
@@ -0,0 +1,13 @@
+import ImageSearchComponent from "../ImageSearchComponent";
+import NavigationBar from "../Navbar";
+// HomePage component
+export default function HomePage() {
+  return (
+    
+      <div className="homepage">
+        <NavigationBar />
+        <ImageSearchComponent />
+      </div>
+
+  );
+}
