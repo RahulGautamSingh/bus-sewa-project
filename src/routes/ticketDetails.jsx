885c322157adc2885c1d2eb1d2dfc72827d0aef8
RahulGautamSingh <rahultesnik@gmail.com>
2024-02-26 12:05:57 +0545

    init ticketDetails page

---
+++ src/routes/ticketDetails.jsx
@@ -0,0 +1,11 @@
+import NavigationBar from "../Navbar";
+
+export default function TicketDetails(){
+    return(
+        <>
+        <div className="ticket-details-container">
+            <NavigationBar/>
+        </div>
+        </>
+    )
+}
